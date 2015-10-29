2d06efed0d24ca650e35e808b9f431ba2b432d8e
Piet Geursen <pietgeursen@gmail.com>
2015-10-29 15:35:48 +1300

    Set interval 200

--- game.js
+++ game.js
@@ -1,15 +1,19 @@
 var conway = require('./conway')
 
-var board = conway.createBoard(10)
-
+var board = conway.createBoard(15)
+//Block
 board[1][1] = true
 board[1][2] = true
 board[2][1] = true
 board[2][2] = true
+//blinker
+board[5][3] = true
+board[5][4] = true
+board[5][5] = true
 
 var stepGame = function(){
     conway.displayBoard(board)
     board = conway.nextBoard(board)
 }
 
-setInterval(stepGame, 300)
+setInterval(stepGame, 200)
