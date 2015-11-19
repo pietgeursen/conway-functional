1fba8551982cabf1525383de04a7daf7058cde1d
Piet Geursen <pietgeursen@gmail.com>
2015-11-20 12:06:49 +1300

    More tidying

--- src/components/board.js
+++ src/components/board.js
@@ -2,10 +2,12 @@ var h = require('virtual-dom/h')
 var row = require('./row')
 
 function render (data) {
-  return h('#board',
+  return h(
+    '#board',
     data.map((r) => {
       return row(r)
     })
-  ) }
+  )
+}
 
 module.exports = render
