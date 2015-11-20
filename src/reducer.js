157078b76fc3f4d7f6c7da403ae3683123325b2e
Piet Geursen <pietgeursen@gmail.com>
2015-11-20 14:00:02 +1300

    Standard style

--- src/reducer.js
+++ src/reducer.js
@@ -1,16 +1,16 @@
-var actions = require('./actions.js');
+var actions = require('./actions.js')
 
-function reducer(state = actions.createBoard(10), action) {
-    switch(action.type){
+function reducer (state = actions.createBoard(10), action) {
+  switch (action.type) {
     case 'SET' :
-        let newState = state.slice()
-        newState[action.r][action.c] = true
-        return newState
+      let newState = state.slice()
+      newState[action.r][action.c] = true
+      return newState
     case 'STEP':
-        return actions.nextBoard(state)
+      return actions.nextBoard(state)
     default:
-        return state
-    }
+      return state
+  }
 }
 
 module.exports = reducer
